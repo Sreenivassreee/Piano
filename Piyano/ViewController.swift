9d56ef5562f3193c3c0b18d1bb1205e066343abd
SreenivasSreee <ksreenivascse@gmail.com>
2021-03-21 22:17:12 +0530

    Piano project

--- Piyano/ViewController.swift
+++ Piyano/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        playSound(sound: sender.currentTitle!)
+    }
+    
+   
 
+    func playSound(sound: String) {
+        guard let url = Bundle.main.url(forResource: sound, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
